96f7b89e3aa5f9d3d8239af72742626830eded36
catefleck <catefleck17@gmail.com>
2025-05-16 21:43:49 -0400

    Update git-lab-program.cc

--- git-lab-program.cc
+++ git-lab-program.cc
@@ -1,1 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Cate Fleckenstein
+ *        Date: 5/16/2025
+ * Description: Add Description
+ */
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
